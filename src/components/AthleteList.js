42d4fc504f162de849a03d5a4b224bab2617d3e8
Stephen Huh <stephenhuh@gmail.com>
2017-09-12 21:37:44 -0700

    align items with twine and implement athletelist

---
+++ src/components/AthleteList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AthleteList extends Component {
+  render() {
+    return (
+      <ol>
+        <li>Harry</li>
+        <li>Sam</li>
+        <li>Stevie</li>
+        <li>Bart</li>
+      </ol>
+    );
+  }
+}
+
+export default AthleteList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// Customized bootstrap styles
+import './theme/styles.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+//
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
